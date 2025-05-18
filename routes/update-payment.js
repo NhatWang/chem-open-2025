d7778594554fa0d1356743e349636792fcd2ded7
Nguyen Nhat Quang <24147110#student.hcmus.edu.vn>
2025-05-18 22:18:19 +0700

    fix mail v5

--- routes/update-payment.js
+++ routes/update-payment.js
@@ -49,9 +49,9 @@ async function sendConfirmationEmail(user, pdfBuffer) {
     BCH Liên chi Hội khoa Hoá học.</p>
 
     <hr style="margin-top: 30px; margin-bottom: 15px; border: none; border-top: 1px solid #ccc;" />
-    <div style="text-align: center; font-size: 30px; color: #000;">
+    <div style="text-align: center; font-size: 25px; color: #000;">
       <p><strong>__________________________________</strong></p>
-      <img src="cid:logoLCH" alt="Logo BCH" style="width: 100px; margin-bottom: 10px;" />
+      <img src="cid:logoLCH" alt="Logo BCH" style="width: 200px; margin-bottom: 10px;" />
       <p style="margin: 5px 0; color: #1a73e8;"><strong>Hội sinh viên Việt Nam – TP. Hồ Chí Minh</strong></p>
       <p style="margin: 0; color: #1a73e8;"><strong>Trường Đại học Khoa học Tự nhiên, ĐHQG - HCM</strong></p>
       <p style="margin: 0 0 10px 0; color: #1a73e8;"><strong>BCH Liên chi Hội khoa Hóa học</strong></p>
